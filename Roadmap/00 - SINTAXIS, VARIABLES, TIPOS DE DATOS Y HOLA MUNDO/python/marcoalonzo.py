5d6467ea822dd91c1d752b502e2a860bac200b28
WinStone <marco.alonzo@protonmail.com>
2024-01-31 18:28:07 -0600

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/marcoalonzo.py
@@ -0,0 +1,35 @@
+# https://www.python.org/
+
+# A pound (#) is the method of writting comments in Python
+
+'''
+Triple single quotes is the way to write 
+a multi-line comments in Python
+'''
+
+# Create a variable and assign value
+abc = 10
+
+# Constant 
+
+# Data types
+# Integer
+num = 10
+
+# String
+name = "marco"
+
+# Boolean
+value = True
+
+# List
+list = [1, 2, 3, 4, 5]
+
+# Tuple
+tuple = (1, 2, 3, 4, 5)
+
+# Dictionary
+identity = {"name": "marco", "age": 35}
+
+# Print "Hello, [name of conding language]""
+print("Hello, Python")
