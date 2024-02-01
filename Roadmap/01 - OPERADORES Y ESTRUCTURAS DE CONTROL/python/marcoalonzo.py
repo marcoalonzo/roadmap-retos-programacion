be56ad1219c1659c732d6d5507b212558c10194c
WinStone <marco.alonzo@protonmail.com>
2024-01-31 21:58:57 -0600

    Started #1 - Python (not yet completed)

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/marcoalonzo.py
@@ -0,0 +1,24 @@
+#  * - Crea ejemplos utilizando todos los tipos de operadores de tu lenguaje:
+
+# Arithmetic Operators
+print("Arithmetic Operators")
+
+a = 10
+b = 20
+
+print(a + b) #addition
+print(a - b) #substraction
+print(a * b) #multiplication
+print(a / b) #division
+print(a % b) #modulus 
+print(a // b) #floor division
+print(a ** b) #exponentiation
+
+#Logical Operators
+print("Logical Operators")
+
+print(True and False) #AND
+print(True or False) #OR
+print(not True) #NOT
+
+#
