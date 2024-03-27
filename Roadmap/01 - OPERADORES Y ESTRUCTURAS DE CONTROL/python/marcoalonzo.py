2dcf8544c5253b65bb77ce4388a367eefa989e5b
Marco Alonzo <marcoalonzo21@gmail.com>
2024-03-27 15:31:54 -0500

    Roadmap/01 python

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/marcoalonzo.py
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/marcoalonzo.py
@@ -1,24 +1,446 @@
 #  * - Crea ejemplos utilizando todos los tipos de operadores de tu lenguaje:
 
 # Arithmetic Operators
-print("Arithmetic Operators")
-
 a = 10
 b = 20
 
-print(a + b) #addition
-print(a - b) #substraction
-print(a * b) #multiplication
-print(a / b) #division
-print(a % b) #modulus 
+print(a + b)  #addition
+print(a - b)  #substraction
+print(a * b)  #multiplication
+print(a / b)  #division
+print(a % b)  #modulus 
 print(a // b) #floor division
 print(a ** b) #exponentiation
 
-#Logical Operators
-print("Logical Operators")
+#All of the below evaluate to False. Everything else will evaluate to True in Python.
+bool(True)
+bool(False)
 
-print(True and False) #AND
-print(True or False) #OR
-print(not True) #NOT
+print(bool(None))
+print(bool(False))
+print(bool(0))
+print(bool(0.0))
+print(bool([]))
+print(bool({}))
+print(bool(()))
+print(bool(''))
+print(bool(range(0)))
+print(bool(set()))
 
-#
+# See Logical Operators and Comparison Operators section for more on booleans.
+
+#Basic Functions
+pow(5, 2)      # 25 --> like doing 5**2
+abs(-50)       # 50
+round(5.46)    # 5
+round(5.468, 2)# 5.47 --> round to nth digit
+bin(512)       # '0b1000000000' -->  binary format
+hex(512)       # '0x200' --> hexadecimal format
+
+#Strings
+type('Hellloooooo') # str
+
+'I\'m thirsty'
+"I'm thirsty"
+"\n" # new line
+"\t" # adds a tab
+
+'Hey you!'[4] # y
+name = 'Marco Alonzo'
+name[4]     # o
+name[:]     # Marco Alonzo
+name[1:]    # arco Alonzo
+name[:1]    # A
+name[-1]    # o
+name[::1]   # Marco Alonzo
+name[::-1]  # oznolA ocraM
+name[0:10:2]# Mro Ao
+# : is called slicing and has the format [ start : end : step ]
+
+'Hi there ' + 'Timmy' # 'Hi there Timmy' --> This is called string concatenation
+'*'*10 # **********
+
+# Basic Functions
+len('turtle') # 6
+
+# Basic Methods
+'  I am alone '.strip()               # 'I am alone' --> Strips all whitespace characters from both ends.
+'On an island'.strip('d')             # 'On an islan' --> # Strips all passed characters from both ends.
+'but life is good!'.split()           # ['but', 'life', 'is', 'good!']
+'Help me'.replace('me', 'you')        # 'Help you' --> Replaces first with second param
+'Need to make fire'.startswith('Need')# True
+'and cook rice'.endswith('rice')      # True
+'still there?'.upper()                # STILL THERE?
+'HELLO?!'.lower()                     # hello?!
+'ok, I am done.'.capitalize()         # 'Ok, I am done.'
+'oh hi there'.count('e')              # 2
+'bye bye'.index('e')                  # 2
+'oh hi there'.find('i')               # 4 --> returns the starting index position of the first occurrence
+'oh hi there'.find('a')               # -1
+'oh hi there'.index('a')              # Raises ValueError
+
+# String Formatting
+name1 = 'Marco'
+name2 = 'Alonzo'
+print(f'Hello there {name1} and {name2}')       # Hello there Marco and Alonzo - Newer way to do things as of python 3.6
+print('Hello there {} and {}'.format(name1, name2))# Hello there Marco and Alonzo
+print('Hello there %s and %s' %(name1, name2))  # Hello there Marco and Alonzo --> you can also use %d, %f, %r for integers, floats, string representations of objects respectively
+
+# Lists
+my_list = [1, 2, '3', True]# We assume this list won't mutate for each example below
+len(my_list)               # 4
+my_list.index('3')         # 2
+my_list.count(2)           # 1 --> count how many times 2 appears
+
+my_list[3]                 # True
+my_list[1:]                # [2, '3', True]
+my_list[:1]                # [1]
+my_list[-1]                # True
+my_list[::1]               # [1, 2, '3', True]
+my_list[::-1]              # [True, '3', 2, 1]
+my_list[0:3:2]             # [1, '3']
+
+# : is called slicing and has the format [ start : end : step ]
+
+# Add to List
+my_list * 2                # [1, 2, '3', True, 1, 2, '3', True]
+my_list + [100]            # [1, 2, '3', True, 100] --> doesn't mutate original list, creates new one
+my_list.append(100)        # None --> Mutates original list to [1, 2, '3', True, 100]          # Or: <list> += [<el>]
+my_list.extend([100, 200]) # None --> Mutates original list to [1, 2, '3', True, 100, 200]
+my_list.insert(2, '!!!')   # None -->  [1, 2, '!!!', '3', True] - Inserts item at index and moves the rest to the right.
+
+' '.join(['Hello','There'])# 'Hello There' --> Joins elements using string as separator.
+
+# Copy a List
+basket = ['apples', 'pears', 'oranges']
+new_basket = basket.copy()
+new_basket2 = basket[:]
+
+# Remove from List
+[1,2,3].pop()    # 3 --> mutates original list, default index in the pop method is -1 (the last item)
+[1,2,3].pop(1)   # 2 --> mutates original list
+[1,2,3].remove(2)# None --> [1,3] Removes first occurrence of item or raises ValueError.
+[1,2,3].clear()  # None --> mutates original list and removes all items: []
+del [1,2,3][0]   # None --> removes item on index 0 or raises IndexError
+
+# Ordering
+[1,2,5,3].sort()         # None --> Mutates list to [1, 2, 3, 5]
+[1,2,5,3].sort(reverse=True) # None --> Mutates list to [5, 3, 2, 1]
+[1,2,5,3].reverse()      # None --> Mutates list to [3, 5, 2, 1]
+sorted([1,2,5,3])        # [1, 2, 3, 5] --> new list created
+my_list = [(4,1),(2,4),(2,5),(1,6),(8,9)]
+sorted(my_list,key=lambda x: int(x[0])) # [(1, 6), (2, 4), (2, 5), (4, 1), (8, 9)] --> sort the list by 1st (0th index) value of the tuple
+list(reversed([1,2,5,3]))# [3, 5, 2, 1] --> reversed() returns an iterator
+
+# Useful operations
+1 in [1,2,5,3]  # True
+min([1,2,3,4,5])# 1
+max([1,2,3,4,5])# 5
+sum([1,2,3,4,5])# 15
+
+# Get First and Last element of a list
+mList = [63, 21, 30, 14, 35, 26, 77, 18, 49, 10]
+first, *x, last = mList
+print(first) #63
+print(last) #10
+
+# Matrix
+matrix = [[1,2,3], [4,5,6], [7,8,9]]
+matrix[2][0] # 7 --> Grab first first of the third item in the matrix object
+
+# Looping through a matrix by rows:
+mx = [[1,2,3],[4,5,6]]
+for row in range(len(mx)):
+	for col in range(len(mx[0])):
+		print(mx[row][col]) # 1 2 3 4 5 6
+    
+# Transform into a list:
+[mx[row][col] for row in range(len(mx)) for col in range(len(mx[0]))] # [1,2,3,4,5,6]
+
+# Combine columns with zip and *:
+[x for x in zip(*mx)] # [(1, 3), (2, 4)]
+
+# List Comprehensions
+# new_list[<action> for <item> in <iterator> if <some condition>]
+a = [i for i in 'hello']                  # ['h', 'e', 'l', 'l', '0']
+b = [i*2 for i in [1,2,3]]                # [2, 4, 6]
+c = [i for i in range(0,10) if i % 2 == 0]# [0, 2, 4, 6, 8]
+
+# Advanced Functions
+list_of_chars = list('Helloooo')                                   # ['H', 'e', 'l', 'l', 'o', 'o', 'o', 'o']
+sum_of_elements = sum([1,2,3,4,5])                                 # 15
+element_sum = [sum(pair) for pair in zip([1,2,3],[4,5,6])]         # [5, 7, 9]
+sorted_by_second = sorted(['hi','you','man'], key=lambda el: el[1])# ['man', 'hi', 'you']
+sorted_by_key = sorted([
+                       {'name': 'Bina', 'age': 30},
+                       {'name':'Andy', 'age': 18},
+                       {'name': 'Zoey', 'age': 55}],
+                       key=lambda el: (el['name']))# [{'name': 'Andy', 'age': 18}, {'name': 'Bina', 'age': 30}, {'name': 'Zoey', 'age': 55}]
+
+# Read line of a file into a list
+with open("myfile.txt") as f:
+  lines = [line.strip() for line in f]
+
+# Dictionaries
+  
+my_dict = {'name': 'Marco Alonzo', 'age': 30, 'magic_power': False}
+my_dict['name']                      # Marco Alonzo
+len(my_dict)                         # 3
+list(my_dict.keys())                 # ['name', 'age', 'magic_power']
+list(my_dict.values())               # ['Marco Alonzo', 30, False]
+list(my_dict.items())                # [('name', 'Marco Alonzo'), ('age', 30), ('magic_power', False)]
+my_dict['favourite_snack'] = 'Grapes'# {'name': 'Marco Alonzo', 'age': 30, 'magic_power': False, 'favourite_snack': 'Grapes'}
+my_dict.get('age')                   # 30 --> Returns None if key does not exist.
+my_dict.get('ages', 0 )              # 0 --> Returns default (2nd param) if key is not found
+
+#Remove key
+del my_dict['name']
+my_dict.pop('name', None)
+
+my_dict.update({'cool': True})                                         # {'name': 'Marco Alonzo', 'age': 30, 'magic_power': False, 'favourite_snack': 'Grapes', 'cool': True}
+{**my_dict, **{'cool': True} }                                         # {'name': 'Marco Alonzo', 'age': 30, 'magic_power': False, 'favourite_snack': 'Grapes', 'cool': True}
+new_dict = dict([['name','Andrei'],['age',32],['magic_power',False]])  # Creates a dict from collection of key-value pairs.
+new_dict = dict(zip(['name','age','magic_power'],['Andrei',32, False]))# Creates a dict from two collections.
+new_dict = my_dict.pop('favourite_snack')                              # Removes item from dictionary.
+
+# Tuples
+
+my_tuple = ('apple','grapes','mango', 'grapes')
+apple, grapes, mango, grapes = my_tuple# Tuple unpacking
+len(my_tuple)                          # 4
+my_tuple[2]                            # mango
+my_tuple[-1]                           # 'grapes'
+
+# Immutability
+my_tuple[1] = 'donuts'  # TypeError
+my_tuple.append('candy')# AttributeError
+
+# Methods
+my_tuple.index('grapes') # 1
+my_tuple.count('grapes') # 2
+
+# Zip
+list(zip([1,2,3], [4,5,6])) # [(1, 4), (2, 5), (3, 6)]
+
+# unzip
+z = [(1, 2), (3, 4), (5, 6), (7, 8)] # Some output of zip() function
+unzip = lambda z: list(zip(*z))
+unzip(z)
+
+# Sets
+
+my_set = set()
+my_set.add(1)  # {1}
+my_set.add(100)# {1, 100}
+my_set.add(100)# {1, 100} --> no duplicates!
+
+new_list = [1,2,3,3,3,4,4,5,6,1]
+set(new_list)           # {1, 2, 3, 4, 5, 6}
+
+my_set.remove(100)      # {1} --> Raises KeyError if element not found
+my_set.discard(100)     # {1} --> Doesn't raise an error if element not found
+my_set.clear()          # {}
+new_set = {1,2,3}.copy()# {1,2,3}
+
+set1 = {1,2,3}
+set2 = {3,4,5}
+set3 = set1.union(set2)               # {1,2,3,4,5}
+set4 = set1.intersection(set2)        # {3}
+set5 = set1.difference(set2)          # {1, 2}
+set6 = set1.symmetric_difference(set2)# {1, 2, 4, 5}
+set1.issubset(set2)                   # False
+set1.issuperset(set2)                 # False
+set1.isdisjoint(set2)                 # False --> return True if two sets have a null intersection.
+
+# Frozenset
+# hashable --> it can be used as a key in a dictionary or as an element in a set.
+<frozenset> = frozenset(<collection>)
+
+# Comparison Operators
+
+==                   # equal values
+!=                   # not equal
+>                    # left operand is greater than right operand
+<                    # left operand is less than right operand
+>=                   # left operand is greater than or equal to right operand
+<=                   # left operand is less than or equal to right operand
+<element> is <element> # check if two operands refer to same object in memory
+
+# Logical Operators
+
+1 < 2 and 4 > 1 # True
+1 > 3 or 4 > 1  # True
+1 is not 4      # True
+not True        # False
+1 not in [2,3,4]# True
+
+if <condition that evaluates to boolean>:
+  # perform action1
+elif <condition that evaluates to boolean>:
+  # perform action2
+else:
+  # perform action3
+	
+# Loops
+
+my_list = [1,2,3]
+my_tuple = (1,2,3)
+my_list2 = [(1,2), (3,4), (5,6)]
+my_dict = {'a': 1, 'b': 2. 'c': 3}
+
+for num in my_list:
+    print(num) # 1, 2, 3
+
+for num in my_tuple:
+    print(num) # 1, 2, 3
+
+for num in my_list2:
+    print(num) # (1,2), (3,4), (5,6)
+
+for num in '123':
+    print(num) # 1, 2, 3
+
+for idx,value in enumerate(my_list):
+    print(idx) # get the index of the item
+    print(value) # get the value
+
+for k,v in my_dict.items(): # Dictionary Unpacking
+    print(k) # 'a', 'b', 'c'
+    print(v) # 1, 2, 3
+
+while <condition that evaluates to boolean>:
+  # action
+  if <condition that evaluates to boolean>:
+    break # break out of while loop
+  if <condition that evaluates to boolean>:
+    continue # continue to the next line in the block
+  
+# waiting until user quits
+msg = ''
+while msg != 'quit':
+    msg = input("What should I do?")
+    print(msg)
+
+# Range 
+    
+range(10)          # range(0, 10) --> 0 to 9
+range(1,10)        # range(1, 10)
+list(range(0,10,2))# [0, 2, 4, 6, 8]
+
+# Enumerate
+
+for i, el in enumerate('helloo'):
+  print(f'{i}, {el}')
+# 0, h
+# 1, e
+# 2, l
+# 3, l
+# 4, o
+# 5, o
+
+# Counter
+  
+from collections import Counter
+colors = ['red', 'blue', 'yellow', 'blue', 'red', 'blue']
+counter = Counter(colors)# Counter({'blue': 3, 'red': 2, 'yellow': 1})
+counter.most_common()[0] # ('blue', 3)
+
+# OrderedDict
+
+from collections import OrderedDict
+# Store each person's languages, keeping # track of who responded first. 
+programmers = OrderedDict()
+programmers['Tim'] = ['python', 'javascript']
+programmers['Sarah'] = ['C++']
+programmers['Bia'] = ['Ruby', 'Python', 'Go']
+
+for name, langs in programmers.items():
+    print(name + '-->')
+    for lang in langs:
+      print('\t' + lang)
+
+# Functions
+      
+args   = (1, 2)
+kwargs = {'x': 3, 'y': 4, 'z': 5}
+some_func(*args, **kwargs) # same as some_func(1, 2, x=3, y=4, z=5)
+
+# Inside Function Definition
+
+def add(*a):
+    return sum(a)
+
+add(1, 2, 3) # 
+
+# Ordering of parameters
+
+def f(*args):                  # f(1, 2, 3)
+def f(x, *args):               # f(1, 2, 3)
+def f(*args, z):               # f(1, 2, z=3)
+def f(x, *args, z):            # f(1, 2, z=3)
+
+def f(**kwargs):               # f(x=1, y=2, z=3)
+def f(x, **kwargs):            # f(x=1, y=2, z=3) | f(1, y=2, z=3)
+
+def f(*args, **kwargs):        # f(x=1, y=2, z=3) | f(1, y=2, z=3) | f(1, 2, z=3) | f(1, 2, 3)
+def f(x, *args, **kwargs):     # f(x=1, y=2, z=3) | f(1, y=2, z=3) | f(1, 2, z=3) | f(1, 2, 3)
+def f(*args, y, **kwargs):     # f(x=1, y=2, z=3) | f(1, y=2, z=3)
+def f(x, *args, z, **kwargs):  # f(x=1, y=2, z=3) | f(1, y=2, z=3) | f(1, 2, z=3)
+
+# Comprehensions
+    
+<list> = [i+1 for i in range(10)]         # [1, 2, ..., 10]
+<set>  = {i for i in range(10) if i > 5}  # {6, 7, 8, 9}
+<iter> = (i+5 for i in range(10))         # (5, 6, ..., 14)
+<dict> = {i: i*2 for i in range(10)}      # {0: 0, 1: 2, ..., 9: 18}
+
+output = [i+j for i in range(3) for j in range(3)] # [0, 1, 2, 1, 2, 3, 2, 3, 4]
+
+# Is the same as:
+output = []
+for i in range(3):
+  for j in range(3):
+    output.append(i+j)
+
+# Scope
+    
+def get_counter():
+    i = 0
+    def out():
+        nonlocal i
+        i += 1
+        return i
+    return out
+
+>>> counter = get_counter()
+>>> counter(), counter(), counter()
+(1, 2, 3)
+
+# Class
+
+class <name>:
+    age = 80 # Class Object Attribute
+    def __init__(self, a):
+        self.a = a # Object Attribute
+
+    @classmethod
+    def get_class_name(cls):
+        return cls.__name__
+    
+# Inheritance
+    
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age  = age
+
+class Employee(Person):
+    def __init__(self, name, age, staff_num):
+        super().__init__(name, age)
+        self.staff_num = staff_num
+
+# Multiple Inheritence 
+
+class A: pass
+class B: pass
+class C(A, B): pass
